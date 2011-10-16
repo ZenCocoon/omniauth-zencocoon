43797513127d98dad67dcf59d6520368cc2263ab
Sébastien Grosjean <public@zencocoon.com>
2011-10-16 03:40:47 +0300

    Fix dependencies

--- lib/ominauth-zencocoon.rb
+++ lib/ominauth-zencocoon.rb
@@ -1,10 +1,2 @@
 require 'omniauth-zencocoon/version'
-require 'omniauth'
-
-module OmniAuth
-  module Strategies
-    autoload :ZenCocoon, 'omniauth/strategies/zencocoon'
-  end
-end
-
-OmniAuth.config.add_camelization 'zencocoon', 'ZenCocoon'
+require 'omniauth/strategies/zencocoon'
